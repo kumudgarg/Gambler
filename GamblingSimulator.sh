119450bddda131555b28a0dd569728d4a6d10712
SandhyaBridgelabz <sandhyapatil364@gmail.com>
2019-11-14 17:44:09 +0530

    after 20 days total amount win and loose by gambler

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,33 +1,42 @@
 #!/bin/bash -x
 BETS=1
-STAKE=100
-initialStake=$STAKE
+DAY=20
+perDayPlayAmount=100
+cashAmount=$perDayPlayAmount
+finalStake=0
 maxWinLimit=0
 minLooseLimit=0
-stake_percent=50
+stakePercent=50
 percentAmountOfStake=0
+totalAmountWinLoose=0
 function gamblingPlay()
 {
 	stackPercent
-	while [ $initialStake -lt $maxWinLimit ] && [ $initialStake -gt $minLooseLimit ]
+	for(( day=1; day <= $DAY; day++ )) 
 	do
-		  
-		if [ $(( RANDOM%2 )) -eq 0 ]
-		then 
-			initialStake=$(( initialStake + BETS ))	
-		else
-			initialStake=$(( initialStake - BETS ))
-		fi
+		cashAmount=$perDayPlayAmount
+		while [ $cashAmount -lt $maxWinLimit ] && [ $cashAmount -gt $minLooseLimit ]
+		do
+			  
+			if [ $(( RANDOM%2 )) -eq 0 ]
+			then 
+				cashAmount=$(( $cashAmount + $BETS ))
+			else
+				cashAmount=$(( $cashAmount - $BETS ))
+			fi
+		done
+		totalAmountWinLoose=$(( $totalAmountWinLoose + ( $cashAmount - $perDayPlayAmount ) ))
 	done 
-	}
+}
 function stackPercent
 {
-	percentAmountOfStake=$(( $(( STAKE * stake_percent ))/100 ))
-	maxWinLimit=$(( STAKE + percentAmountOfStake ))
-	minLooseLimit=$(( STAKE - percentAmountOfStake )) 
+	percentAmountOfStake=$(( $(( perDayPlayAmount * stakePercent ))/100 ))
+	maxWinLimit=$(( perDayPlayAmount + percentAmountOfStake ))
+	minLooseLimit=$(( perDayPlayAmount - percentAmountOfStake )) 
 }
 gamblingPlay
 echo "final stake are $initialStake "
 echo "maximum win limit $maxWinLimit "
 echo "minimum loose limit $minLooseLimit "
+echo "total amount win / loose $totalAmountWinLoose"
 
