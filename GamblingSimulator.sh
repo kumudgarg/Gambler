2e4b7fad972b24175ffb1cd5b4b224848872026b
SandhyaBridgelabz <sandhyapatil364@gmail.com>
2019-11-14 15:50:17 +0530

    on 50% of stake gambler resign that day

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,30 +2,32 @@
 BETS=1
 STAKE=100
 initialStake=$STAKE
-max_win_limit=0
-min_loose_limit=0
+maxWinLimit=0
+minLooseLimit=0
 stake_percent=50
+percentAmountOfStake=0
 function gamblingPlay()
 {
 	stackPercent
-	while [ $initialStake -lt $max_win_limit ] && [ $initialStake -gt $min_loose_limit ]
+	while [ $initialStake -lt $maxWinLimit ] && [ $initialStake -gt $minLooseLimit ]
 	do
 		  
 		if [ $(( RANDOM%2 )) -eq 0 ]
 		then 
-			(( initialStake++ ))	
+			initialStake=$(( initialStake + BETS ))	
 		else
-			(( initialStake-- ))
+			initialStake=$(( initialStake - BETS ))
 		fi
 	done 
 	}
 function stackPercent
 {
-	max_win_limit=$(( $(( STAKE * $(( STAKE + stake_percent ))  ))/100 )) 
-	min_loose_limit=$(( $(( STAKE * $(( STAKE - stake_percent )) ))/100 ))
+	percentAmountOfStake=$(( $(( STAKE * stake_percent ))/100 ))
+	maxWinLimit=$(( STAKE + percentAmountOfStake ))
+	minLooseLimit=$(( STAKE - percentAmountOfStake )) 
 }
 gamblingPlay
 echo "final stake are $initialStake "
-echo "maximum win limit $max_win_limit "
-echo "minimum loose limit $min_loose_limit "
+echo "maximum win limit $maxWinLimit "
+echo "minimum loose limit $minLooseLimit "
 
