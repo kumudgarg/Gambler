5853f657ee7d6e3e8bc2caeb56c3c9726af893cb
SandhyaBridgelabz <sandhyapatil364@gmail.com>
2019-11-15 10:18:32 +0530

    luckiest day and unluckiest day with amount of gambler done

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -11,7 +11,12 @@ percentAmountOfStake=0
 totalAmountWinLoose=0
 wonDays=0
 looseDays=0
+maxDayAmt=0
+minDayAmt=0
+luckiestDay=0
+unluckiestDay=0
 declare -A dayInfo
+declare -A luckyDay
 function gamblingPlay()
 {
 	stackPercent
@@ -52,16 +57,40 @@ function dayWiseWonLoose()
 	amountWin=$(( wonDays * percentAmountOfStake ))
 	amountLoose=$((looseDays * percentAmountOfStake ))
 }
+function luckyUnluckyDay()
+{
+	daysAmountArr[0]=0
+	for (( day=1; day<=$DAY; day++ ))
+	do 
+		daysAmountArr[ $day ]=$(( ${daysAmountArr[ $(($day-1)) ]} + ${dayInfo["day $day"]} ))
+	done
+	for (( day=1; day<=$DAY; day++ ))
+	do 
+		if [ $maxDayAmt -gt ${daysAmountArr[ $day ]} ]
+		then
+			minDayAmt=${daysAmountArr[ $day ]}
+			(( luckiestDay++ ))
+		else
+			maxDayAmt=${daysAmountArr[ $day ]}
+			(( unluckiestDay++ ))
+		fi
+	done
+}
+function echoInfo()
+{
+	echo "final stake are $initialStake "
+	echo "maximum win limit $maxWinLimit "
+	echo "minimum loose limit $minLooseLimit "
+	echo "total amount win / loose $totalAmountWinLoose"
+	echo " ${dayInfo[@]}"
 
+	echo "win days are $wonDays and total amount of $wonDays are $amountWin "
+	echo "win days are $looseDays and total amount of $looseDays are $amountLoose " 
+	echo "  ${daysAmountArr[@]} $luckiestDay $unluckiestDay "
+	echo "luckiestday $luckiestDay  has $maxDayAmt  amount and  unluckiestday $unluckiestDay  has $minDayAmt amount" 
+}
 gamblingPlay
 dayWiseWonLoose
-echo "final stake are $initialStake "
-echo "maximum win limit $maxWinLimit "
-echo "minimum loose limit $minLooseLimit "
-echo "total amount win / loose $totalAmountWinLoose"
-echo " ${dayInfo[@]}"
-
-echo "win days are $wonDays and total amount of $wonDays are $amountWin "
-echo "win days are $looseDays and total amount of $looseDays are $amountLoose " 
- 
+luckyUnluckyDay
+echoInfo
 
